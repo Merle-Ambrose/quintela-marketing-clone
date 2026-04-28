3be963f61204f2e20b44babd9fdc72574a6b7c12
potikhomi <potikhomi@gmail.com>
2026-04-28 12:15:23 -0400

    fix mobile grid styles in SystemIntegrations.css

--- src/components/SystemIntegrations/SystemIntegrations.js
+++ src/components/SystemIntegrations/SystemIntegrations.js
@@ -10,7 +10,7 @@ function SystemIntegrations() {
         <h2 className="text-center fw-bold mb-4 text-secondary">
           Systems Integrations
         </h2>
-        <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-4 justify-content-center align-items-center">
+        <div className="row row-cols-3 row-cols-sm-3 row-cols-md-4 row-cols-lg-6 g-4 justify-content-center align-items-center">
           {SYSTEM_LOGOS.map(({ src, alt }) => (
             <div key={src} className="col d-flex justify-content-center">
               <img src={src} alt={alt} className="integrationCompaniesImg" />
